70da6a24c9d342c4b344b610f207601562b92fee
"Antonio Sanchez” " <antonio@feedbackdialog.com>
2013-08-12 11:53:56 +0300

    added dozer mapper file

--- services/src/main/java/net/sf/gazpachosurvey/services/impl/AbstractPersistenceService.java
+++ services/src/main/java/net/sf/gazpachosurvey/services/impl/AbstractPersistenceService.java
@@ -3,11 +3,8 @@ package net.sf.gazpachosurvey.services.impl;
 import java.io.Serializable;
 import java.util.List;
 
-import net.sf.gazpachosurvey.domain.core.Label;
-import net.sf.gazpachosurvey.domain.core.LabelSet;
 import net.sf.gazpachosurvey.domain.support.Persistable;
 import net.sf.gazpachosurvey.dto.Identifiable;
-import net.sf.gazpachosurvey.dto.LabelSetDTO;
 import net.sf.gazpachosurvey.repository.qbe.SearchParameters;
 import net.sf.gazpachosurvey.repository.support.GenericRepository;
 import net.sf.gazpachosurvey.services.PersistenceService;
@@ -19,7 +16,7 @@ public class AbstractPersistenceService<T_DAO extends GenericRepository<T, ID>, 
         implements PersistenceService<D, ID> {
 
     protected final GenericRepository<T, ID> repository;
-     final Class<D> typeParameterClass;
+    final Class<D> typeParameterClass;
     @Autowired
     private Mapper mapper;
 
@@ -37,12 +34,11 @@ public class AbstractPersistenceService<T_DAO extends GenericRepository<T, ID>, 
     @Override
     public D findOne(ID id) {
         T entity = repository.findOne(id);
-
-       D dto = mapper.map(entity, typeParameterClass);
+        D dto = mapper.map(entity, typeParameterClass, "labelSet.skeleton");
         return dto;
     }
 
-
+    @Override
     public long count() {
         // TODO Auto-generated method stub
         return 0;
--- services/src/test/java/net/sf/gazpachosurvey/services/SurveyServiceTest.java
+++ services/src/test/java/net/sf/gazpachosurvey/services/SurveyServiceTest.java
@@ -5,7 +5,6 @@ import net.sf.gazpachosurvey.dto.LabelDTO;
 import net.sf.gazpachosurvey.dto.LabelSetDTO;
 import net.sf.gazpachosurvey.dto.PageDTO;
 import net.sf.gazpachosurvey.dto.SurveyDTO;
-import net.sf.gazpachosurvey.repository.LabelSetRepository;
 import net.sf.gazpachosurvey.types.Language;
 
 import org.junit.Test;
@@ -17,8 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("postgres")
-@ContextConfiguration(locations = { "classpath:/jpa-context.xml",
-        "classpath:/services-context.xml" })
+@ContextConfiguration(locations = { "classpath:/jpa-context.xml", "classpath:/services-context.xml" })
 /*
  * @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
  * DirtiesContextTestExecutionListener.class,
@@ -46,25 +44,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
                 .title("Slightly Trickier Sample Survey")
                 .description(
                         "<p>This is a <strong><em>sample survey</em></strong> designed for testing GazpachoSurvey.</p><p>One of the first things you'll need to do, in order to make this work properly, is to fix up the &quot;reference to previous answers&quot; codes in the first two questions of Group 2.  These questions contain (INSERTANS:1X2X3) code that needs the numbers changed to match questions 1 and 3 of Group 1. In order to find this number out, browse to question 1 of Group 1, and then copy the code from the URL bar in your web browser, starting from the number after &quot;SID=&quot;</p><p><strong><em>IE:</em></strong> 29975&amp;gid=2&amp;qid=4</p><p>Then, replace the &quot;&amp;gid=&quot; with an X, and replace the &quot;&amp;qid=&quot; with an X. So you should have:</p><p>29975X2X4</p><p>Then, you can replace the number in the curly brackets of question 1, group 2, so that it says:</p><p>(INSERTANS:29975X2X4)</p><p>Do the same with the other codes in the questions.</p><p>More information on using the answers to previous questions in your questions is available in the documentation.</p>")
-                .welcomeText(
-                        "Thank you for taking the time to participate in this survey.")
+                .welcomeText("Thank you for taking the time to participate in this survey.")
                 .surveyLanguageSettingsEnd().build();
 
         Integer surveyId = surveyService.addSurvey(survey);
         assertThat(surveyId).isNotNull();
 
-        PageDTO page = PageDTO.with().pageLanguageSettingsStart()
-                .title("Page 1").pageLanguageSettingsEnd().build();
+        PageDTO page = PageDTO.with().pageLanguageSettingsStart().title("Page 1").pageLanguageSettingsEnd().build();
         Integer id = pageService.addPage(surveyId, page);
         assertThat(id).isNotNull();
 
-        page = PageDTO.with().pageLanguageSettingsStart().title("Page 2")
-                .pageLanguageSettingsEnd().build();
+        page = PageDTO.with().pageLanguageSettingsStart().title("Page 2").pageLanguageSettingsEnd().build();
         id = pageService.addPage(surveyId, page);
         assertThat(id).isNotNull();
 
-        LabelSetDTO labelSet = LabelSetDTO.with().language(Language.EN)
-                .name("Feelings").build();
+        LabelSetDTO labelSet = LabelSetDTO.with().language(Language.EN).name("Feelings").build();
         Integer labelSetId = labelSetService.addLabelSet(labelSet);
 
         LabelDTO label = LabelDTO.with().title("Agree strongly").build();
@@ -81,8 +75,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
         label = LabelDTO.with().title("Disagree strongly").build();
         labelSetService.addLabel(labelSetId, label);
-        
-        LabelSetDTO winner = labelSetService.findOne(5);
+
+        LabelSetDTO winner = labelSetService.findOne(1);
 
         System.out.println("fin" + winner.getName());
 
