e905c2a00d3ffcae54ef6eeaa111d3dd5fda7ae8
"Antonio Sanchez” " <antonio@feedbackdialog.com>
2013-08-14 15:10:12 +0300

    Completed

--- services/src/main/java/net/sf/gazpachosurvey/aspects/ProfilingAdvise.java
+++ services/src/main/java/net/sf/gazpachosurvey/aspects/ProfilingAdvise.java
@@ -4,69 +4,52 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StopWatch;
+import org.springframework.util.StopWatch.TaskInfo;
 
+/**
+ * 
+ * @author antonio
+ * @see http
+ *      ://thoughtforge.net/713/creating-a-profiling-aspect-with-spring-aop-and
+ *      -aspectj/
+ * 
+ */
 public class ProfilingAdvise {
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(ProfilingAdvise.class);
+    private static final Logger logger = LoggerFactory.getLogger(ProfilingAdvise.class);
 
-    private ProfilerThreadLocal profilerThreadLocal = new ProfilerThreadLocal();
+    private final ProfilerThreadLocal profilerThreadLocal = new ProfilerThreadLocal();
 
-    public Object doProfilingxx(ProceedingJoinPoint proceedingJoinPoint)
-            throws Throwable {
-        // http://thoughtforge.net/713/creating-a-profiling-aspect-with-spring-aop-and-aspectj/
-        StopWatch watch = new StopWatch();
-        watch.start();
-        Object obj = proceedingJoinPoint.proceed();
-        watch.stop();
-
-        String message = new StringBuffer(proceedingJoinPoint.getTarget()
-                .getClass().getName()).append(".")
-                .append(proceedingJoinPoint.getSignature().getName())
-                .toString();
-        logger.debug(message + " " + watch.getTotalTimeMillis());
-        return obj;
-    }
-
-    public Object doProfiling(ProceedingJoinPoint proceedingJoinPoint)
-            throws Throwable {
-
+    public Object doProfiling(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         String taskName = createTaskName(proceedingJoinPoint);
         StopWatch taskTimer = profilerThreadLocal.get();
         try {
             taskTimer.start(taskName);
-
             return proceedingJoinPoint.proceed();
         } finally {
             taskTimer.stop();
-
-            if (taskTimer.getTaskCount() == 0) {
-                doLogging(proceedingJoinPoint.getTarget().getClass(),
-                        taskTimer.prettyPrint());
-                profilerThreadLocal.remove();
-            }
+            doLogging(taskTimer.getLastTaskInfo());
+            profilerThreadLocal.remove();
         }
     }
 
-    private String createTaskName(ProceedingJoinPoint proceedingJoinPoint) {
-        return new StringBuffer(proceedingJoinPoint.getTarget().getClass()
-                .getName()).append(".")
-                .append(proceedingJoinPoint.getSignature().getName())
-                .toString();
+    private void doLogging(TaskInfo lastTaskInfo) {
+        logger.debug("Method : {} took {} ms", lastTaskInfo.getTaskName(), lastTaskInfo.getTimeMillis());
     }
 
-    private void doLogging(Class<?> clazz, String prettyPrint) {
-        logger.info("\n\n" + prettyPrint + "\n");
+    private String createTaskName(ProceedingJoinPoint proceedingJoinPoint) {
+        return new StringBuffer(proceedingJoinPoint.getTarget().getClass().getSimpleName()).append(".")
+                .append(proceedingJoinPoint.getSignature().getName()).toString();
     }
 
     private class ProfilerThreadLocal extends ThreadLocal<StopWatch> {
 
         @Override
         protected StopWatch initialValue() {
-            StopWatch stopWatch = new StopWatch(Thread.currentThread()
-                    .getName());
+            StopWatch stopWatch = new StopWatch(Thread.currentThread().getName());
             stopWatch.setKeepTaskList(false);
             return stopWatch;
         }
+
     }
 }
