f2e34ff7ff57e39abfaf6f6fcc8316799a32b9d7
Antonio Sanchez” <antonio@feedbackdialog.com>
2013-09-19 15:09:13 +0300

    Added questionnaires-ui skeleton

---
+++ questionnaires-ui/src/main/java/net/sf/gazpachosurvey/questionnaires/MyVaadinUI.java
@@ -0,0 +1,40 @@
+package net.sf.gazpachosurvey.questionnaires;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("mytheme")
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI {
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "net.sf.gazpachosurvey.questionnaires.AppWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        setContent(layout);
+
+        Button button = new Button("Click Me");
+        button.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        layout.addComponent(button);
+    }
+
+}
