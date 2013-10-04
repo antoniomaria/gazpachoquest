bfa38b17e5357c4a75e1bf4ef6ce0461338ec54b
antoniomaria <antoniomariasanchez@gmail.com>
2013-10-04 20:42:05 +0300

    Added test for MailMessageTemplateServiceTest

--- services/src/test/java/net/sf/gazpachosurvey/services/MailMessageTemplateServiceTest.java
+++ services/src/test/java/net/sf/gazpachosurvey/services/MailMessageTemplateServiceTest.java
@@ -1,5 +1,6 @@
 package net.sf.gazpachosurvey.services;
 
+import static org.fest.assertions.api.Assertions.assertThat;
 import net.sf.gazpachosurvey.dto.MailMessageTemplateDTO;
 import net.sf.gazpachosurvey.dto.MailMessageTemplateLanguageSettingsDTO;
 import net.sf.gazpachosurvey.types.Language;
@@ -7,14 +8,25 @@ import net.sf.gazpachosurvey.types.Language;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("postgres")
-@ContextConfiguration(locations = { "classpath:/jpa-context.xml",
+@ContextConfiguration(locations = { "classpath:/jpa-test-context.xml",
+        "classpath:/datasource-test-context.xml",
         "classpath:/services-context.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
+@DatabaseSetup("MailMessageTemplateService-dataset.xml")
 public class MailMessageTemplateServiceTest {
 
     @Autowired
@@ -25,10 +37,6 @@ public class MailMessageTemplateServiceTest {
 
     @Test
     public void saveTest() {
-//        userService.save(UserDTO.with().firstName("temporal.support")
-//                .lastName("support").email("support.temporal@gazpacho.net")
-//                .build());
-
         MailMessageTemplateDTO mailMessageTemplate = MailMessageTemplateDTO
                 .with()
                 .language(Language.EN)
@@ -42,13 +50,17 @@ public class MailMessageTemplateServiceTest {
 
         MailMessageTemplateLanguageSettingsDTO languageSettings = MailMessageTemplateLanguageSettingsDTO
                 .with()
-                .subject("Tu encuesta mofificada")
+                .subject("Tu encuesta")
                 .body("Estimado Sr. $lastname, <br> Has sido invitado para participar en esta encuesta <br>"
                         + "Nos dedicas 15 minutos para realizar la encuesta?, puedes interrumpirla y completarla más tarde si es necesario"
                         + "<a href=\"\">Click aqui</a> para empezar").build();
-        
-        service.saveTranslation(mailMessageTemplate.getId(), Language.ES,  languageSettings);
-       MailMessageTemplateDTO localizedMailMessageTemplate = service.findOne(mailMessageTemplate.getId(), Language.ES);
-       
+
+        service.saveTranslation(mailMessageTemplate.getId(), Language.ES,
+                languageSettings);
+        MailMessageTemplateDTO localizedMailMessageTemplate = service.findOne(
+                mailMessageTemplate.getId(), Language.ES);
+        assertThat(
+                localizedMailMessageTemplate.getLanguageSettings().getSubject())
+                .isEqualTo(languageSettings.getSubject());
     }
 }
