84382dd7fb8381218b10c0448b33f3259969d015
antoniomaria <antoniomariasanchez@gmail.com>
2013-10-13 21:40:02 +0300

    Added dbpopulator application

---
+++ dbpopulator/src/main/java/net/sf/gazpachosurvey/dbpopulator/DBPopulatorRunner.java
@@ -0,0 +1,16 @@
+package net.sf.gazpachosurvey.dbpopulator;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DBPopulatorRunner {
+
+    public static void main(String ... args){
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "dbpopulator-datasource-context.xml", "dbpopulator-jpa-context.xml", "services-context.xml");
+        DBPopulator populator = (DBPopulator) ctx
+                .getBean(DBPopulator.class);
+        populator.populate();
+        ctx.close();
+    }
+}
---
+++ domain/src/main/java/net/sf/gazpachosurvey/types/Gender.java
@@ -0,0 +1,5 @@
+package net.sf.gazpachosurvey.types;
+
+public enum Gender {
+    MALE, FEMALE
+}
