ccb4b0f5b46d08da615789ef2d2b4b7afbbfb707
antoniomaria <antoniomariasanchez@gmail.com>
2013-10-17 18:01:27 +0300

    Proof of concept displaying data from datasource using vaadin

--- questionnaires-ui/src/main/java/net/sf/gazpachosurvey/questionnaires/QuestionnairesUI.java
+++ questionnaires-ui/src/main/java/net/sf/gazpachosurvey/questionnaires/QuestionnairesUI.java
@@ -1,5 +1,10 @@
 package net.sf.gazpachosurvey.questionnaires;
 
+import java.util.List;
+
+import net.sf.gazpachosurvey.dto.SurveyDTO;
+import net.sf.gazpachosurvey.services.SurveyService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -20,15 +25,13 @@ public class QuestionnairesUI extends UI {
     private static final long serialVersionUID = 1265851857862002747L;
 
     @Autowired
-    private transient HelloWorldComponent component;
-    
+    private transient SurveyService surveyService;
     
     @Override
     protected void init(VaadinRequest request) {
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
-        Label label = new Label(component.getMessage());
         
         Button button = new Button("Click Me Again!");
         button.addClickListener(new Button.ClickListener() {
@@ -37,8 +40,14 @@ public class QuestionnairesUI extends UI {
                 layout.addComponent(new Label("Thank you for clicking"));
             }
         });
-        layout.addComponent(label);
         layout.addComponent(button);
+        
+        List<SurveyDTO> surveys = surveyService.findAll();
+        
+        for (SurveyDTO surveyDTO : surveys) {
+            Label label = new Label(surveyDTO.getLanguageSettings().getTitle());
+            layout.addComponent(label);
+        }
     }
 
 }
