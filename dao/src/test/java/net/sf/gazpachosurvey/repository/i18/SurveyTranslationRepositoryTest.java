543053a0221ca5fa48d26d9f19e0de7d82764f84
antoniomaria <antoniomariasanchez@gmail.com>
2013-11-14 17:54:25 +0200

    New test added

---
+++ dao/src/test/java/net/sf/gazpachosurvey/repository/i18/SurveyTranslationRepositoryTest.java
@@ -0,0 +1,63 @@
+package net.sf.gazpachosurvey.repository.i18;
+
+import java.util.List;
+
+import net.sf.gazpachosurvey.domain.core.Survey;
+import net.sf.gazpachosurvey.domain.i18.SurveyTranslation;
+import net.sf.gazpachosurvey.repository.qbe.SearchParameters;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/jpa-test-context.xml",
+        "classpath:/datasource-test-context.xml" })
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class })
+@DatabaseSetup("SurveyTranslationRepository-dataset.xml")
+public class SurveyTranslationRepositoryTest {
+
+    @Autowired
+    private SurveyTranslationRepository surveyTranslationRepository;
+
+
+    @Test
+    public void saveTest() {
+        System.out.println("fin");
+        
+    // List<SurveyTranslation> translations = surveyTranslationRepository.findByTranslatedEntityId(Survey.with().id(2).build());
+        
+        List<String> translations = surveyTranslationRepository.findByTranslatedEntityId();
+        
+        System.out.println(translations);
+    }
+
+
+    @Test
+    public void findByExample() {
+        System.out.println("fin");
+        
+     //   List<SurveyTranslation> translations = surveyTranslationRepository.findByTranslatedEntityId(Survey.with().id(3).build());
+        
+        
+        SurveyTranslation entity = SurveyTranslation.with().translatedEntityId(2).build();
+        
+        List<SurveyTranslation> translations = surveyTranslationRepository.findByExample(entity , new SearchParameters());
+        
+        System.out.println(translations);
+    }
+
+    
+}
