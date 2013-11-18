da7e71ae5ad68aa4dec4fcca236800be6e1b1560
antoniomaria <antoniomariasanchez@gmail.com>
2013-11-18 13:37:00 +0200

    Enable jackson feature

--- rest/src/main/java/net/sf/gazpachosurvey/rest/ApplicationConfig.java
+++ rest/src/main/java/net/sf/gazpachosurvey/rest/ApplicationConfig.java
@@ -3,6 +3,7 @@ package net.sf.gazpachosurvey.rest;
 import net.sf.gazpachosurvey.rest.auth.AuthorizationRequestFilter;
 import net.sf.gazpachosurvey.rest.resources.runtime.QuestionnairResource;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
@@ -12,6 +13,8 @@ public class ApplicationConfig extends ResourceConfig {
     public ApplicationConfig(){
         register(RequestContextFilter.class);
         register(RolesAllowedDynamicFeature.class);
+        register(JacksonFeature.class);
+        
         register(AuthorizationRequestFilter.class);
         register(QuestionnairResource.class);
         
