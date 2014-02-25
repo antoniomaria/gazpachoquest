8fe880bbc393d88d6d4098034f036b90e0d3e418
antonio maria <antoniomariasanchez@gmail.com>
2014-02-25 21:27:24 +0200

    Enviroment Discover on progress

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-gazpachosurvey
+GazpachoQuest
 ==============
+
+Set up
+
+-Dspring.profiles.active=standalone -DGAZPACHO_APP={\"jdbc.url\":\"jdbc:postgresql://localhost:5432/gazpachoquest\",\"jdbc.username\":\"postgres\",\"jdbc.driver.class\":\"org.postgresql.Driver\",\"jdbc.password\":\"admin\"}
--- rest/src/main/java/net/sf/gazpachoquest/rest/EnviromentDiscovery.java
+++ rest/src/main/java/net/sf/gazpachoquest/rest/EnviromentDiscovery.java
@@ -1,7 +1,12 @@
 package net.sf.gazpachoquest.rest;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContextInitializer;
@@ -14,17 +19,41 @@ public class EnviromentDiscovery implements ApplicationContextInitializer<Config
 
     private static final Logger logger = LoggerFactory.getLogger(EnviromentDiscovery.class);
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final String GAZPACHO_APP_KEY = "GAZPACHO_APP";
+
     @Override
     public void initialize(ConfigurableApplicationContext ctx) {
         logger.info("Application running local");
         ConfigurableEnvironment environment = ctx.getEnvironment();
+        String instanceInfoString = environment.getProperty(GAZPACHO_APP_KEY);
 
-        if (environment.acceptsProfiles("development")) {
+        Map<String, String> environmentProperties = parseInstanceInfo(instanceInfoString);
+
+        if (environment.acceptsProfiles("standalone")) {
+            String driverClass = environmentProperties.get("jdbc.driver.class");
             Properties properties = new Properties();
             // Create mongo service properties for development environment
             properties.put("hostname", "localhost");
             environment.getPropertySources().addFirst(new PropertiesPropertySource("properties", properties));
         }
+
+    }
+
+    private Map<String, String> parseInstanceInfo(String instanceInfoString) {
+        Map<String, String> properties = new HashMap<String, String>();
+        if (StringUtils.isNotBlank(instanceInfoString)) {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                properties = mapper.readValue(instanceInfoString, new TypeReference<HashMap<String, String>>() {
+                });
+            } catch (Exception e) {
+                logger.error("Errors found parsing GAZPACHO_APP json string", e);
+                throw new RuntimeException(e);
+            }
+        }
+        return properties;
     }
 
 }
