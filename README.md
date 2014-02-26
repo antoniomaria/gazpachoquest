0f026a230c9bf6af0e4d1202e25baae21f2d19f0
Antonio Sanchez” <antonio@feedbackdialog.com>
2014-02-26 15:59:53 +0200

    Enviroment Discover on progress

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ GazpachoQuest
 
 Set up
 
--Dspring.profiles.active=standalone -DGAZPACHO_APP={\"jdbc.url\":\"jdbc:postgresql://localhost:5432/gazpachoquest\",\"jdbc.username\":\"postgres\",\"jdbc.driver.class\":\"org.postgresql.Driver\",\"jdbc.password\":\"admin\"}
+-Dspring.profiles.active=standalone,postgres -DGAZPACHO_APP={\"jdbc.url\":\"jdbc:postgresql://localhost:5432/gazpachoquest\",\"jdbc.username\":\"postgres\",\"jdbc.driver.class\":\"org.postgresql.Driver\",\"jdbc.password\":\"admin\"}
--- rest/src/main/java/net/sf/gazpachoquest/rest/EnviromentDiscovery.java
+++ rest/src/main/java/net/sf/gazpachoquest/rest/EnviromentDiscovery.java
@@ -1,8 +1,8 @@
 package net.sf.gazpachoquest.rest;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -11,8 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.io.support.ResourcePropertySource;
 
 // ConfigurableWebApplicationContext
 public class EnviromentDiscovery implements ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -29,15 +30,49 @@ public class EnviromentDiscovery implements ApplicationContextInitializer<Config
         ConfigurableEnvironment environment = ctx.getEnvironment();
         String instanceInfoString = environment.getProperty(GAZPACHO_APP_KEY);
 
-        Map<String, String> environmentProperties = parseInstanceInfo(instanceInfoString);
+        PropertySourcesPlaceholderConfigurer propertyHolder = new PropertySourcesPlaceholderConfigurer();
 
+        Map<String, String> environmentProperties = parseInstanceInfo(instanceInfoString);
+        if (environment.acceptsProfiles("postgres")) {
+            try {
+                environment.getPropertySources().addLast(
+                        new ResourcePropertySource("classpath:/database/postgres.properties"));
+            } catch (IOException e) {
+                throw new IllegalStateException("postgres.properties not found in classpath", e);
+            }
+        } else if (environment.acceptsProfiles("mysql")) {
+            try {
+                environment.getPropertySources().addLast(
+                        new ResourcePropertySource("classpath:/database/mysql.properties"));
+            } catch (IOException e) {
+                throw new IllegalStateException("postgres.properties not found in classpath", e);
+            }
+        } else {
+            try {
+                environment.getPropertySources().addLast(
+                        new ResourcePropertySource("classpath:/database/hsql.properties"));
+            } catch (IOException e) {
+                throw new IllegalStateException("postgres.properties not found in classpath", e);
+            }
+            environment.setActiveProfiles("test", "standalone");
+        }
+        propertyHolder.setEnvironment(environment);
+        ctx.addBeanFactoryPostProcessor(propertyHolder);
+        ctx.refresh();
+        /*-
         if (environment.acceptsProfiles("standalone")) {
             String driverClass = environmentProperties.get("jdbc.driver.class");
             Properties properties = new Properties();
-            // Create mongo service properties for development environment
             properties.put("hostname", "localhost");
+            try {
+                environment.getPropertySources().addFirst(
+                        new ResourcePropertySource("classpath:/database/postgres.properties"));
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             environment.getPropertySources().addFirst(new PropertiesPropertySource("properties", properties));
-        }
+        }*/
 
     }
 
