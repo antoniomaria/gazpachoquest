aa564f8938c5220241ccdc5209f354e9bedfa265
Antonio Sanchez <antonio.sanchez-berrocal@landisgyr.com>
2015-08-11 12:52:20 +0300

    Log changes

--- persistence/dao/src/main/java/net/sf/gazpachoquest/repository/dynamic/QuestionnaireAnswersRepositoryImpl.java
+++ persistence/dao/src/main/java/net/sf/gazpachoquest/repository/dynamic/QuestionnaireAnswersRepositoryImpl.java
@@ -197,7 +197,7 @@ public class QuestionnaireAnswersRepositoryImpl implements QuestionnaireAnswersR
 
     private void processQuestion(final JPADynamicTypeBuilder builder, final Question question) {
         Assert.notNull(question.getId(), String.format("Persist question first {%s}", question));
-        Assert.notNull(question.getType(), String.format("Not questionType for question: {%s}", question));
+        Assert.notNull(question.getType(), String.format("Question with id = %s not loaded", question.getId()));
 
         final QuestionType questionType = question.getType();
         if (!questionType.hasSubquestions()) {
