0e9db629c8db4cd10ac8455203265294e661cbeb
Antonio Sanchez <antonio.sanchez-berrocal@landisgyr.com>
2015-08-11 11:17:15 +0300

    Test more robust

--- facades/src/test/java/net/sf/gazpachoquest/facades/ResearchFacadeTest.java
+++ facades/src/test/java/net/sf/gazpachoquest/facades/ResearchFacadeTest.java
@@ -31,8 +31,8 @@ import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import com.github.springtestdbunit.annotation.DbUnitConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:/jpa-test-context.xml", "classpath:/datasource-test-context.xml",
-        "classpath:/services-context.xml", "classpath:/components-context.xml", "classpath:/facades-context.xml" })
+@ContextConfiguration(locations = { "classpath:/jpa-test-context.xml", "classpath:/datasource-test-context.xml", "classpath:/services-context.xml",
+        "classpath:/components-context.xml", "classpath:/facades-context.xml" })
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
 @DatabaseSetup("ResearchFacade-dataset.xml")
 @DatabaseTearDown("ResearchFacade-dataset.xml")
@@ -53,16 +53,17 @@ public class ResearchFacadeTest extends AbstractShiroTest {
 
     @Before
     public void setUp() {
-        questionnaireAnswersRepository.activeAllAnswers();
+        if (!answersActivated()) {
+            questionnaireAnswersRepository.activeAllAnswers();
+        }
     }
-
 
     @Test
     public void findPaginatedTest() {
-        PageDTO<ResearchDTO> page = researchFacade.findPaginated(1, 5);
+        final PageDTO<ResearchDTO> page = researchFacade.findPaginated(1, 5);
         assertThat(page.getTotalPages()).isEqualTo(1);
         assertThat(page.getTotalElements()).isEqualTo(1);
-        ResearchDTO research = page.getElements().get(0);
+        final ResearchDTO research = page.getElements().get(0);
         assertThat(research).isEqualTo(ResearchDTO.with().id(57).build());
         assertThat(research.getName()).isNotEmpty();
         assertThat(research.getCreatedBy()).isNotNull();
@@ -70,25 +71,20 @@ public class ResearchFacadeTest extends AbstractShiroTest {
         assertThat(research.getQuestionnaireDefinition().getCreatedBy()).isNull();
         assertThat(research.getQuestionnaireDefinition().getSections()).isEmpty();
     }
-    
+
     @Test
     public void saveTest() {
-        QuestionnaireDefinitionDTO questionnaireDefinition = QuestionnaireDefinitionDTO.with().id(7).build();
+        final QuestionnaireDefinitionDTO questionnaireDefinition = QuestionnaireDefinitionDTO.with().id(7).build();
         ResearchDTO research = ResearchDTO.with().name("My research").type(ResearchAccessType.BY_INVITATION).build();
         research.setQuestionnaireDefinition(questionnaireDefinition);
         research = researchFacade.save(research);
         assertThat(research.isNew()).isFalse();
     }
 
-    @After
-    public void tearDown() {
-        jdbcTemplate.update("delete from questionnaire_answers_7");
-    }
-
     @Before
     public void setUpSubject() {
-        Subject subjectUnderTest = createNiceMock(Subject.class);
-        User support = User.with().id(1).build();
+        final Subject subjectUnderTest = createNiceMock(Subject.class);
+        final User support = User.with().id(1).build();
         expect(subjectUnderTest.getPrincipal()).andReturn(support).anyTimes();
         replay(subjectUnderTest);
         // 2. Bind the subject to the current thread:
@@ -99,5 +95,14 @@ public class ResearchFacadeTest extends AbstractShiroTest {
     public void tearDownSubject() {
         // 3. Unbind the subject from the current thread:
         clearSubject();
+    }
+
+    private boolean answersActivated() {
+        try {
+            jdbcTemplate.execute("SELECT * FROM questionnaire_answers_7");
+            return true;
+        } catch (final Exception e) {
+            return false;
+        }
     }
 }
